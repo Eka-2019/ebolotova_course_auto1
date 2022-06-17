643e1ff69d7ef901ea8b3a196be1a18a35612000
ekaterinabolotova <bolotova-katya@yandex.ru>
2022-06-17 10:11:43 -0400

    models commit

---
+++ src/test/java/runner/TestSuite.java
@@ -0,0 +1,10 @@
+package runner;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("tests.junit")
+public class TestSuite {
+
+}
